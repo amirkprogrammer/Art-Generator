6c06e2ba42fa3427dbadad87e6feac730512c35e
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-07-11 18:13:09 -0700

    add style and artist parameters

---
+++ Art Generator/Models/Styles.swift
@@ -0,0 +1,56 @@
+//
+//  Styles.swift
+//  Art Generator
+//
+//  Created by Amir Kabiri on 7/11/24.
+//
+
+import Foundation
+
+enum ImageStyle: String, CaseIterable {
+    case none
+    case abstract = "abstract"
+    case cartoon
+    case comic
+    case expressionism
+    case impressionism
+    case popArt = "pop art"
+    case realism
+    case renaissance
+    case surrealism
+    
+    var description: String {
+        self != .none ? "an image in the style of " + self.rawValue + " " : ""
+    }
+}
+
+enum ImageMedium: String, CaseIterable {
+    case none
+    case digital = "digital art"
+    case oil = "oil painting"
+    case pastel
+    case photo
+    case spray = "spray paint"
+    case watercolor
+    
+    var description: String {
+        self != .none ? "using the medium of " + self.rawValue + " " : ""
+    }
+}
+
+enum Artist: String, CaseIterable {
+    case none
+    case dali = "Salvador Dali"
+    case davinci = "Leonardo da Vinci"
+    case matisse = "Henri Matisse"
+    case monet = "Claud Monet"
+    case picasso = "Pable Picasso"
+    case pollock = "Jackson Pollock"
+    case vangogh = "Vincent van Gogh"
+    case warhol = "Andy Warhol"
+    
+    var description: String {
+        self != .none ? "similar to the worksof " + self.rawValue + " " : ""
+    }
+    
+}
