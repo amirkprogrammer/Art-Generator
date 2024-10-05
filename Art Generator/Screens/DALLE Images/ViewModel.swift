d65019df78a270ef12202bc320f2e4467d4b338f
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-10-05 16:53:29 -0700

    presenting images in an HStack

---
+++ Art Generator/Screens/DALLE Images/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Art Generator
+//
+//  Created by Amir Kabiri on 10/5/24.
+//
+
+import Foundation
---
+++ Art Generator/Screens/DALLEImagesView.swift
@@ -0,0 +1,24 @@
+//
+//  DalleImagesVIew.swift
+//  Art Generator
+//
+//  Created by Amir Kabiri on 10/4/24.
+//
+
+import SwiftUI
+
+struct DALLEImagesView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    DALLEImagesView()
+}
